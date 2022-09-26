8a3ef90cf9e1d400d6f6f4cbdb038ebe550c24f5
Barbara796 <barbara6.dossantos@gmail.com>
2022-09-26 15:43:11 -0300

    código live parte 2

--- canvas.js
+++ canvas.js
@@ -1,28 +1,47 @@
 function desenharCanvas() {
-    tabuleiro.lineWidth=8
-    tabuleiro.lineCap="round"
-    tabuleiro.lineJoin="round"
-    tabuleiro.fillStyle= "#F3F5FC"
-    tabuleiro.strokeStyle = "#0A3871"
-    tabuleiro.fillRect(0,0,1200,800)
-    tabuleiro.beginPath();
-    tabuleiro.moveTo(650,500)
-    tabuleiro.lineTo(900,500)
-    tabuleiro.stroke()
-    tabuleiro.closePath()
-  }
+  tela.lineWidth=8
+  tela.lineCap="round"
+  tela.lineJoin="round"
+  tela.fillStyle= "#F3F5FC"
+  tela.strokeStyle = "#0A3871"
+  tela.fillRect(0,0,1200,800)
+  tela.beginPath();
+  tela.moveTo(650,500)
+  tela.lineTo(900,500)
+  tela.stroke()
+  tela.closePath()
+}
 
-  function desenharLinhas() {
-    tabuleiro.lineWidth=6
-    tabuleiro.lineCap="round"
-    tabuleiro.lineJoin="round"
-    tabuleiro.strokeStyle = "#0A3871"
-    tabuleiro.beginPath()
-    let largura=600/palavraSecreta.length
-    for (let i=0;i<palavraSecreta.length;i++){
-      tabuleiro.moveTo(500+(largura*i),640)
-      tabuleiro.lineTo(550+(largura*i),640)
-    }
-    tabuleiro.stroke()
-    tabuleiro.closePath()
+function desenharLinhas() {
+  tela.lineWidth=6
+  tela.lineCap="round"
+  tela.lineJoin="round"
+  tela.strokeStyle = "#0A3871"
+  tela.beginPath()
+  let largura=600/palavraSecreta.length
+  for (let i=0;i<palavraSecreta.length;i++){
+    tela.moveTo(500+(largura*i),640)
+    tela.lineTo(550+(largura*i),640)
   }
+  tela.stroke()
+  tela.closePath()
+}
+
+
+function escreverLetraCorreta(index){
+    tela.font = "bold 52px Inter"
+    tela.lineCap = "round"
+    tela.fillStyle = "#0A3871"
+    tela.lineWidth = 6
+    let largura= 600/palavraSecreta.length
+    tela.fillText(palavraSecreta[index],505 + (largura * index),620 )
+    
+}
+
+function escreverLetraIncorreta(letra, erros){
+  tela.font = "bold 40px Inter"
+  tela.lineCap = "round"
+  tela.fillStyle = "#0A3871"
+  tela.lineWidth = 6
+  tela.fillText(letra,520 + (40 *(10 - erros)),720,40 )
+}
--- forca.js
+++ forca.js
@@ -1,7 +1,9 @@
 let palavras = ['ALURA', 'FORCA', 'HTML', 'ORACLE', 'JAVASCRIPT', 'LOGICA', 'PROGRAMA', 'DESAFIO'];
-let tabuleiro = document.getElementById('forca').getContext('2d');
+let tela = document.getElementById('forca').getContext('2d');
 let palavraSecreta = "";
 
+let letras = []
+let erros = 8
 
 function escolherPalavraSecreta() {
     let palavra = palavras[Math.floor(Math.random() * palavras.length)]
@@ -10,10 +12,62 @@ function escolherPalavraSecreta() {
     return palavra
   }
 
-  function iniciaJogo(){
+  function verificarLetra(key){
+    let estado = false
+    if(key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key) ){
+       letras.push(key)
+       console.log(key)
+       console.log(letras)
+       return estado
+    }
+    else{
+      estado = true
+      letras.push(key)
+      console.log(key)
+      console.log(letras, "if true")
+      return estado
+    }
+  }
+
+  function adicionarLetraIncorreta(){
+    erros -= 1
+  }
+
+  //captar a tecla digitada
+  //verificar se é uma letra
+  // saber se a palavra secreta inclui a letra digitada
+  // escrever letra correta
+  // escrever letra incorreta 
+  // contar os erros
+
+
+  
+function iniciaJogo(){
+   
     document.getElementById("div-desaparece").style.display = 'none';
     escolherPalavraSecreta()
     
+
     desenharCanvas()
     desenharLinhas()
-  }
+
+    document.onkeydown = (e) => {
+
+        let letra = e.key.toUpperCase()
+
+        if(verificarLetra(letra) && palavraSecreta.includes(letra)){
+          for(let i = 0; i < palavraSecreta.length; i++){
+            if(palavraSecreta[i] === letra){
+              escreverLetraCorreta(i)
+            }
+          }
+        }
+        else{
+          adicionarLetraIncorreta(letra)
+          escreverLetraIncorreta(letra, erros)
+
+        }
+      }
+
+    }
+  
