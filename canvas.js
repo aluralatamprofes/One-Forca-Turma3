332a76f81cc26c75d58c93daf507a1eb25ef0247
Alura Latam Profes <98755017+weslleyabastos@users.noreply.github.com>
2022-09-26 18:06:12 -0300

    forca jogo completo

--- canvas.js
+++ canvas.js
@@ -1,47 +1,117 @@
 function desenharCanvas() {
-  tela.lineWidth=8
-  tela.lineCap="round"
-  tela.lineJoin="round"
-  tela.fillStyle= "#F3F5FC"
-  tela.strokeStyle = "#0A3871"
-  tela.fillRect(0,0,1200,800)
-  tela.beginPath();
-  tela.moveTo(650,500)
-  tela.lineTo(900,500)
-  tela.stroke()
-  tela.closePath()
+  tablero.lineWidth=8
+  tablero.lineCap="round"
+  tablero.lineJoin="round"
+  tablero.fillStyle= "#F3F5FC"
+  tablero.strokeStyle = "#0A3871"
+  tablero.fillRect(0,0,1200,800)
+  tablero.beginPath();
+  tablero.moveTo(650,500)
+  tablero.lineTo(900,500)
+  tablero.stroke()
+  tablero.closePath()
 }
 
 function desenharLinhas() {
-  tela.lineWidth=6
-  tela.lineCap="round"
-  tela.lineJoin="round"
-  tela.strokeStyle = "#0A3871"
-  tela.beginPath()
+  tablero.lineWidth=6
+  tablero.lineCap="round"
+  tablero.lineJoin="round"
+  tablero.strokeStyle = "#0A3871"
+  tablero.beginPath()
   let largura=600/palavraSecreta.length
   for (let i=0;i<palavraSecreta.length;i++){
-    tela.moveTo(500+(largura*i),640)
-    tela.lineTo(550+(largura*i),640)
+    tablero.moveTo(500+(largura*i),640)
+    tablero.lineTo(550+(largura*i),640)
   }
-  tela.stroke()
-  tela.closePath()
+  tablero.stroke()
+  tablero.closePath()
+}
+function escreverLetraCorreta(index) {
+  tablero.font = 'bold 52px Inter';
+  tablero.lineWidth=6
+  tablero.lineCap="round"
+  tablero.lineJoin="round"
+  tablero.fillStyle= "#0A3871"
+  let largura=600/palavraSecreta.length
+  tablero.fillText(palavraSecreta[index],505+(largura*index),620)
+  tablero.stroke()
 }
 
-
-function escreverLetraCorreta(index){
-    tela.font = "bold 52px Inter"
-    tela.lineCap = "round"
-    tela.fillStyle = "#0A3871"
-    tela.lineWidth = 6
-    let largura= 600/palavraSecreta.length
-    tela.fillText(palavraSecreta[index],505 + (largura * index),620 )
-    
+function escreverLetraIncorreta(letra, errorsLeft) {
+  tablero.lineWidth=6
+  tablero.font = 'bold 40px Inter';
+  tablero.lineCap="round"
+  tablero.lineJoin="round"
+  tablero.fillStyle="#0A3871"
+  tablero.fillText(letra,535+(40*(10-errorsLeft)),710,40)
 }
 
-function escreverLetraIncorreta(letra, erros){
-  tela.font = "bold 40px Inter"
-  tela.lineCap = "round"
-  tela.fillStyle = "#0A3871"
-  tela.lineWidth = 6
-  tela.fillText(letra,520 + (40 *(10 - erros)),720,40 )
+function desenharForca(pontos) {
+  tablero.lineWidth=8
+  tablero.lineCap="round"
+  tablero.lineJoin="round"
+  tablero.strokeStyle = "#0A3871"
+  if(pontos===8){
+  //poste lateral
+  tablero.moveTo(700,500)
+  tablero.lineTo(700,100)
+  }
+  if(pontos===7){//teto 
+  tablero.moveTo(850,100)
+  tablero.lineTo(700,100)
+  }
+  if(pontos===6){//corda
+  tablero.moveTo(850,100)
+  tablero.lineTo(850,171)
+  }
+  if(pontos===5){//para cara
+  tablero.moveTo(900,230)
+  tablero.arc(850,230,50,0,Math.PI*2)
+  }
+  if(pontos===4){//para corpo
+  tablero.moveTo(850,389)
+  tablero.lineTo(850,289)
+  }
+  if(pontos===3){//para perna esquerda
+  tablero.moveTo(850,389)
+  tablero.lineTo(800,450)
+  }
+  if(pontos===2){//para perna direita
+  tablero.moveTo(850,389)
+  tablero.lineTo(890,450)
+  }
+  if(pontos===1){//para mão izquerda
+  tablero.moveTo(850,330)
+  tablero.lineTo(800,389)
+  }
+  if(pontos===0){//para mão direita
+  tablero.moveTo(850,330)
+  tablero.lineTo(890,389)
+  }
+  tablero.stroke()
+  tablero.closePath()
+}
+
+function exibirDerrota() {
+  tablero.font = ' bold 42px Inter';
+  tablero.lineWidth=6
+  tablero.lineCap="round"
+  tablero.lineJoin="round"
+  tablero.fillStyle="red"
+  tablero.fillText("Fim de jogo!",930,320)
+}
+
+function exibirVitoria() {
+  tablero.font = 'bold 42px Inter';
+  tablero.lineWidth=6
+  tablero.lineCap="round"
+  tablero.lineJoin="round"
+  tablero.fillStyle="green"
+  tablero.fillText("Ganhou,",950,320)
+  tablero.fillText("Parabéns!",930,360)
+  setTimeout( recarregar , 1000)
+}   
+
+function recarregar(){
+  location.reload(); 
 }
